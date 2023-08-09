3304c2545c0ec3fd32854317766fdfeca4055136
Kaisei <me@kaisei.dev>
2023-08-09 15:41:37 +0900

    test: footer & header test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +6,7 @@ import 'package:strawberry_chocolate/presentation/widgets/header.dart';
 import 'package:strawberry_chocolate/presentation/widgets/footer.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Home widget test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
     // Check if the Header widget is found
@@ -23,5 +16,21 @@ void main() {
     // Check if the Footer widget is found
     expect(find.byType(FooterWidget),
         findsOneWidget); // Replace FooterWidget with the actual type of your footer widget
+  });
+
+  testWidgets("Header widget test", (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: HeaderWidget(),
+    ));
+    expect(find.text('todo'), findsOneWidget);
+    expect(find.text('memo'), findsOneWidget);
+  });
+
+  testWidgets("Footer widget test", (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: FooterWidget(),
+    ));
+    expect(find.byIcon(Icons.home_filled), findsOneWidget);
+    expect(find.byIcon(Icons.settings), findsOneWidget);
   });
 }
